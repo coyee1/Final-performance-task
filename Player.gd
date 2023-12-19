7ba97cbd6a3f3831d5c9740b97f96c2e99804923
Colin <coyee1@s.sfusd.edu>
2023-12-19 12:01:59 -0800

    sound effects complete

--- Player.gd
+++ Player.gd
@@ -1,14 +1,28 @@
 extends Area2D
 
+@onready var bgmusic = $"../bgmusic"
+@onready var enemy_hit = $"../enemy_hit"
+
 @onready var speed = 400
+
 func _process(delta):
 	var velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		velocity.x -= 10
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		velocity.x += 10
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		velocity.y -= 10
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		velocity.y += 10
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+
+
+
+
+func _on_enemy_body_entered(body):
+	hide()
+	enemy_hit
